5a1c4c5cb9198b4e6d1945b69b4be40d16221a98
juasai <juasai@proton.me>
2025-09-09 21:56:41 -0500

    add image to descripcion product

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,7 +4,8 @@ import ItemCount from "../ItemCount/ItemCount";
 const ItemDetail = ({ product }) => {
   return (
     <div>
-      <h2>{product.name}</h2>
+      <img src={product.image} alt={product.title} style={{ width: '200px', height: '200px' }} />
+      <h2>{product.title}</h2>
       <p>{product.description}</p>
       <p>Precio: ${product.price}</p>
       <ItemCount stock={10} initial={1} />
