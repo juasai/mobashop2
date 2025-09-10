d79b534e19278828aceb9d22db1e6702ad86bc2e
juasai <juasai@proton.me>
2025-09-09 21:50:46 -0500

    show image products

--- src/components/ItemList/Item.jsx
+++ src/components/ItemList/Item.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const Item = ({ product }) => {
   return (
     <div>
-      <h3>{product.name}</h3>
+      <img src={product.image} alt={product.name} style={{ width: '100px', height: '100px' }} />
+      <h3>{product.title}</h3>
       <p>Precio: ${product.price}</p>
       <Link to={`/producto/${product.id}`}>Ver Detalle</Link>
     </div>
