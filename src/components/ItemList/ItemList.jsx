68f7c6c21638d14943a3e77c954c558c070088e0
juasai <juasai@proton.me>
2025-09-22 00:08:26 -0500

    change background

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,12 +3,12 @@ import Item from "./Item";
 
 const ItemList = ({ products }) => {
   return (
-    <div style={{ 
-      display: 'grid', 
-      gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', 
-      gap: '20px', 
-      padding: '20px', 
-      backgroundColor: '#f9f9f9' 
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+      gap: '20px',
+      padding: '20px',
+      //backgroundColor: '#f9f9f9'
     }}>
       {products.map((product) => (
         <Item key={product.id} product={product} />
